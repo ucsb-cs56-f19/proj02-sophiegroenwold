065cbf7dfb48b16078a9b8f17e7885abf9c2600f
Sophie Groenwold <sophiegroenwold@umail.ucsb.edu>
2019-11-12 17:12:57 -0800

    SG fix javadoc comments

--- src/main/java/hello/GithubOrgMembershipService.java
+++ src/main/java/hello/GithubOrgMembershipService.java
@@ -53,6 +53,7 @@ public class GithubOrgMembershipService implements MembershipService {
      * is current logged in user has role
      * 
      * @param roleToTest "member" or "admin"
+     * @param oauthToken oauth token
      * @return if the current logged in user has that role
      */
 
