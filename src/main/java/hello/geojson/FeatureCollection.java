d9689f161f063bb8d2b065a70c6c2bbfc62f0ff6
Sophie Groenwold <sophiegroenwold@umail.ucsb.edu>
2019-11-17 00:34:01 -0800

    SG - create Metadata object and display JSON on results

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 public class FeatureCollection {
   public String type;
+  public Metadata metadata;
 
   private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
 
