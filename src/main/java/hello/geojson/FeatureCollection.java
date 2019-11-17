cc82ff0699bdba1fd1aabe2ae02ce0987906c0a8
Sophie Groenwold <sophiegroenwold@umail.ucsb.edu>
2019-11-17 00:55:06 -0800

    SG display Feature and Properties JSON

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.List;
 
 public class FeatureCollection {
   public String type;
   public Metadata metadata;
+  public List<Feature> features;
 
   private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
 
