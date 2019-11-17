a91c66fc460638e722091ea25bf5abcc94c5ac14
Sophie Groenwold <sophiegroenwold@umail.ucsb.edu>
2019-11-16 18:57:04 -0800

    SG travis ci

--- src/test/java/hello/HomePageTest.java
+++ src/test/java/hello/HomePageTest.java
@@ -38,7 +38,6 @@ public class HomePageTest {
                 .andExpect(content().contentType("text/html;charset=UTF-8"));
     }
 
-
     @Test
     public void getHomePage_BootstrapLoaded() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
