94e991cc89fc84f6b3b0e7d00e3b10a0306683db
sophiegroenwold <sophiegroenwold@umail.ucsb.edu>
2019-12-03 15:05:25 -0800

    SG - add @DeleteMapping to LocationsController

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -18,6 +18,8 @@ import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping; 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -67,5 +69,14 @@ public class LocationsController {
       locationRepository.save(location);
       model.addAttribute("locations", locationRepository.findAll());
       return "locations/index";
+    }
+
+    @DeleteMapping("/locations/delete/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+    Location location = locationRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid courseoffering Id:" + id));
+    locationRepository.delete(location);
+    model.addAttribute("locations", locationRepository.findAll());
+    return "locations/index";
     }
 }
