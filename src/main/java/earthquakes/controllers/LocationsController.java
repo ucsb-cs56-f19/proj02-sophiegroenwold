3e09225b38ec45420011048ca2ee49992dc298d5
sophiegroenwold <sophiegroenwold@umail.ucsb.edu>
2019-12-02 21:52:39 -0800

    SG - update LocationsController

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -15,7 +15,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping; 
 import java.util.Map;
 import java.util.HashMap;
 //import earthquakes.geojson.FeatureCollection;
